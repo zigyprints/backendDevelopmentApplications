77e04b4cdff78d3187f4fd97440a7dedec6735b1
ice-009 <21bma009@nith.ac.in>
2023-07-30 22:55:49 +0530

    sqlite used in place of mongo

--- src/index.ts
+++ src/index.ts
@@ -1,42 +1,52 @@
-import express, {Request, Response} from 'express'
-const http = require('http')
-const bodyParser = require('body-parser')
-const mongoose = require('mongoose')
-const cors= require('cors')
-const app= express()
-app.use(bodyParser.json())
-app.use((req: Request, res: Response, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  });
-mongoose.connect('mongodb+srv://ice-009:ice-009@cluster0.n4xny3y.mongodb.net/', {
-  useNewUrlParser: true,
-  useUnifiedTopology: true,
-});
-app.use(cors())
+import express, { Request, Response } from 'express';
+import http from 'http';
+import bodyParser from 'body-parser';
+import sqlite3 from 'sqlite3';
+import { open } from 'sqlite';
+import cors from 'cors';
 
+const app = express();
+app.use(bodyParser.json());
+app.use(cors());
 
-const todoSchema = new mongoose.Schema({
-    heading: { type: String, required: true },
-    content: { type: String },
-  });
-  const todoModel = mongoose.model('todo', todoSchema)
-  app.use((req: Request, res: Response, next) => {
-    console.log(`${req.method} ${req.url}`);
-    next();
-  });
+async function setupDatabase() {
+  try {
+    const db = await open({
+      filename: './mydatabase.sqlite',
+      driver: sqlite3.Database,
+    });
+
+    await db.exec(`
+      CREATE TABLE IF NOT EXISTS todos (
+        id INTEGER PRIMARY KEY AUTOINCREMENT,
+        heading TEXT NOT NULL,
+        content TEXT
+      );
+    `);
+
+    return db;
+  } catch (error) {
+    console.error('Error setting up the database:', error);
+    throw error;
+  }
+}
+
+const initializeApp = async () => {
+  const db = await setupDatabase();
+
   app.get('/api/todos', async (req: Request, res: Response) => {
     try {
-      const todos = await todoModel.find();
+      const todos = await db.all('SELECT * FROM todos;');
       res.json(todos);
     } catch (error) {
       res.status(500).json({ message: 'Internal Server Error' });
     }
   });
+
   app.get('/api/todos/:id', async (req: Request, res: Response) => {
     const id = req.params.id;
     try {
-      const todo = await todoModel.findById(id);
+      const todo = await db.get('SELECT * FROM todos WHERE id = ?;', [id]);
       if (todo) {
         res.json(todo);
       } else {
@@ -50,20 +60,29 @@ const todoSchema = new mongoose.Schema({
   app.post('/api/todos', async (req: Request, res: Response) => {
     const { heading, content } = req.body;
     try {
-      const newTodo = await todoModel.create({ heading, content });
+      const result = await db.run('INSERT INTO todos (heading, content) VALUES (?, ?);', [
+        heading,
+        content,
+      ]);
+      const newTodo = { id: result.lastID, heading, content };
       res.status(201).json(newTodo);
     } catch (error) {
       res.status(500).json({ message: 'Internal Server Error' });
     }
   });
-  
+
   app.put('/api/todos/:id', async (req: Request, res: Response) => {
     const id = req.params.id;
     const { heading, content } = req.body;
     try {
-      const todo = await todoModel.findByIdAndUpdate(id, { heading, content }, { new: true });
-      if (todo) {
-        res.json(todo);
+      const result = await db.run('UPDATE todos SET heading = ?, content = ? WHERE id = ?;', [
+        heading,
+        content,
+        id,
+      ]);
+      if (result.changes > 0) {
+        const updatedTodo = { id: parseInt(id), heading, content };
+        res.json(updatedTodo);
       } else {
         res.status(404).json({ message: 'Todo not found' });
       }
@@ -71,12 +90,12 @@ const todoSchema = new mongoose.Schema({
       res.status(500).json({ message: 'Internal Server Error' });
     }
   });
-  
+
   app.delete('/api/todos/:id', async (req: Request, res: Response) => {
     const id = req.params.id;
     try {
-      const todo = await todoModel.findByIdAndDelete(id);
-      if (todo) {
+      const result = await db.run('DELETE FROM todos WHERE id = ?;', [id]);
+      if (result.changes > 0) {
         res.sendStatus(204);
       } else {
         res.status(404).json({ message: 'Todo not found' });
@@ -85,9 +104,13 @@ const todoSchema = new mongoose.Schema({
       res.status(500).json({ message: 'Internal Server Error' });
     }
   });
-  
-  app.listen(8080, () => {
-    console.log(`Server is running on http://localhost:8080}`);
+
+  const port = 8080;
+  app.listen(port, () => {
+    console.log(`Server is running on http://localhost:${port}`);
   });
+};
 
-
+initializeApp().catch((error) => {
+  console.error('Error initializing the app:', error);
+});
