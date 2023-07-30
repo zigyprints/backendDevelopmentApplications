b7fc4a075893e69826beddcd019f55a4e137b071
sunny-dubey <97658562+sunny-dubey@users.noreply.github.com>
2023-07-31 01:06:43 +0530

    working server

--- src/server.ts
+++ src/server.ts
@@ -12,6 +12,7 @@ mongoose
     .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
     .then(() => {
         Logging.info('Connected to MongoDB');
+        startService();
     })
     .catch((error) => {
         Logging.error('Unable to Connect: ');
@@ -19,3 +20,33 @@ mongoose
     });
 
 // start the server only if the Mongo is connected
+
+const startService = () => {
+    router.use((req, res, next) => {
+        // log the request
+        Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] `);
+
+        res.on('finish', () => {
+            Logging.info(`Incoming -> Method: [${req.method}] - Url: [${req.url}] - IP: [${req.socket.remoteAddress}] - Status: [${res.statusCode}] `);
+        });
+
+        next();
+    });
+
+    router.use(express.urlencoded({ extended: true }));
+    router.use(express.json());
+
+    // routes
+
+    // healthcheck
+    router.get('/ping', (req, res, next) => res.status(200).json({ message: 'pong' }));
+
+    // error handling
+    router.use((req, res, next) => {
+        const error = new Error('not found');
+        Logging.error(error);
+        return res.status(404).json({ message: error.message });
+    });
+
+    http.createServer(router).listen(config.server.port, () => Logging.info(`Server is running on port ${config.server.port}.`));
+};
