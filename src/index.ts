eb41ee2a97c758de75aa0baa60e4385b92bb224d
ice-009 <21bma009@nith.ac.in>
2023-07-30 18:18:43 +0530

    basic whole setup importing downloading done

--- src/index.ts
+++ src/index.ts
@@ -1,1 +1,16 @@
-console.log("armaan")
+const express = require('express')
+const http = require('http')
+const bodyParser = require('body-parser')
+const cookieParser = require('cookie-parser')
+const coompression = require('compression')
+const cors= require('cors')
+
+const app= express()
+app.use(cors({credentials:true}))
+
+app.use(bodyParser.json())
+const server = http.createServer(app)
+
+server.listen(8080, ()=>{
+    console.log("listening on port 8080")
+})
