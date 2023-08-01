335e8efb4bb20a2fa7419fb61d5cc3e13731cbe6
Disha <bidisha2125@gmail.com>
2023-08-02 02:46:37 +0530

    Added the CREATE todo route

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,5 +1,9 @@
 import express from "express";
+import { createTodo } from '../controllers/todoController'
 
 const router = express.Router();
+
+//create todo
+router.post('/', createTodo);
 
 export default router;
