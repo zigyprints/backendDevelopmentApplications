214a1148f801db77d84d7e7b4a9d3ac384c5d188
Disha <bidisha2125@gmail.com>
2023-08-02 03:00:08 +0530

    Created router to GET All the TODOS

--- src/routes/todoRoutes.ts
+++ src/routes/todoRoutes.ts
@@ -1,9 +1,12 @@
 import express from "express";
-import { createTodo } from '../controllers/todoController'
+import { createTodo, getAllTodos } from "../controllers/todoController";
 
 const router = express.Router();
 
 //create todo
 router.post('/', createTodo);
+
+//get all todos
+router.get("/", getAllTodos);
 
 export default router;
