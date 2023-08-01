c98f54e86aa8959807873266bf97607ae4ccf66d
Disha <bidisha2125@gmail.com>
2023-08-02 02:47:35 +0530

    Added CORS and body-parser

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,21 @@
 import express, { Request, Response } from "express";
 import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
 
 //paths
 import connectDB from './config/dbConnection';
 import todoRoutes from './routes/todoRoutes';
 
+//creating express app
 const app = express();
 
 //configuring env
 dotenv.config();
+
+//middleware
+app.use(cors());
+app.use(bodyParser.json());
 
 //Db connection
 connectDB();
@@ -26,7 +33,7 @@ app.use('/api/v1/todos', todoRoutes);
 //port declaration
 const PORT = process.env.PORT || 8080;
 
-//server
+//server running
 app.listen(PORT, () => {
   console.log(`Listening on port ${PORT}`);
 });
