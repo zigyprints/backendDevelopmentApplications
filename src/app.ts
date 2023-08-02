e922a12d609a2271bb368b47fa3112034d7cf284
Sk-Akibuzzaman-Jony <skajony2002@gmail.com>
2023-08-02 22:20:20 +0530

    Implemented CORS

--- src/app.ts
+++ src/app.ts
@@ -1,18 +1,30 @@
 import express from "express";
 import dotenv from 'dotenv';
+import cors from 'cors';
 import todoRoutes from './routes/todoRoutes'
+
 
 dotenv.config(); 
 
 
 const app = express();
 
+//Adding a list of allowed origins for cors
+const allowedOrigins = ['http://localhost:3000'];
+
+const options: cors.CorsOptions = {
+  origin: allowedOrigins
+};
+
+//Then passing the option(allowed origins) to cors 
+app.use(cors(options));
+
 app.use(express.json());
 
 //API Routes
 app.use('/todo', todoRoutes);
 
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 3001;
 
 app.listen(PORT, () => {
   console.log(`Server is running on port ${PORT}`);
