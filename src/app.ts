1446735d96a9e04e62a25d1802765062f1c3a458
Vansh Mittal <mittalvansh69@gmail.com>
2023-08-26 23:00:05 +0530

    Added / route!

--- src/app.ts
+++ src/app.ts
@@ -24,6 +24,10 @@ app.use(errorHandler);
 
 app.use("/api/v1/tasks", tasks);
 
+app.get("/", (req, res) => {
+  res.send("Welcome to the TodoList API");
+});
+
 const PORT = process.env.PORT || 5000;
 
 app.listen(PORT, () => {
