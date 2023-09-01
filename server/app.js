d2a0bd074e4fd8054d36882d447e4822d530115d
Tamish <tamishbajaj360@gmail.com>
2023-09-01 12:47:57 +0530

    Server rewritten to handel errors and middlewares

--- server/app.js
+++ server/app.js
@@ -1,10 +1,41 @@
-const express=require('express');
-const app=express()
+const express= require('express');
+const app=express();
 
-app.get('/', (req,res)=>{
-    res.send();
-})
+const tasks=require('./routes/tasks');
+const connectDB=require('./db/connect')
+const notFound=require('./middleware/not-found')
+const errorHandlerMiddleware=require('./middleware/error-handler')
 
-app.listen(3000, ()=>{
-    
-})
+require('dotenv').config()
+//middleware
+
+app.use(express.static('./public'))
+app.use(express.json())   // If we dont use this we will not have data in req.body()
+
+//routes
+
+
+// app.get('api/v1/tasks') ->get all the tasks  [1] kind of route i need here jhsjceb
+
+app.use('/api/v1/tasks',tasks)
+
+// app.post('api/v1/tasks')  ->create a new task
+
+// When the route doesnot exist
+app.use(notFound)
+app.use(errorHandlerMiddleware)
+
+const port=process.env.PORT || 3000;
+
+const start=async()=>{
+    try{
+        await connectDB(process.env.MONGO_URI)
+        app.listen(port,console.log(`Server listening on port ${port} ...`))
+    }catch(error){
+        console.log(error)
+    }
+
+}
+
+start();
+
