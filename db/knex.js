4e7f644b198e0fb06efdc60be68f0e0fc99050a6
Sanskar soni <sanskarsoni.cse23@jecrc.ac.in>
2023-09-03 21:18:15 +0530

    adding db folder

---
+++ db/knex.js
@@ -0,0 +1,11 @@
+const knex = require('knex');
+
+const connectedKnex = knex({
+    client: 'sqlite3',
+    connection: {
+        filename: 'db.sqlite3'
+    },
+    useNullAsDefault: true,
+})
+
+module.exports = connectedKnex;
