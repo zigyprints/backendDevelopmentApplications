dffed3dcf830aaef73133175cc685ae21ce4d639
rahulyadav826870 <rahulyadav826870@gmail.com>
2023-09-03 16:34:54 +0530

    Create Todo model with title, description, and completion fields

--- Backend/src/models/todo.model.ts
+++ Backend/src/models/todo.model.ts
@@ -1,1 +1,16 @@
-//model
+import mongoose, { Schema, Document } from 'mongoose';
+
+export interface Todo extends Document {
+  title: string;
+  description: string;
+  completed: boolean;
+}
+
+const todoSchema: Schema = new Schema({
+  title: { type: String, required: true },
+  description: { type: String, required: true },
+  completed: { type: Boolean, default: false },
+});
+
+ const Todo=mongoose.model<Todo>('todo', todoSchema);
+ export default Todo
