788400c7361cbe427a3d077c27da7b940fe97293
Souvik chakraborty <22souvik22@gmail.com>
2023-10-02 15:17:24 +0530

    Authentication and Client setup

--- Server/src/Routes/userRoutes.ts
+++ Server/src/Routes/userRoutes.ts
@@ -1,8 +1,11 @@
 import express,{ Express,Request,Response } from 'express';
-import {registerUser} from '../controllers/userControls.js'
+import {registerUser,loginUser,finduser,getalluser} from '../controllers/userControls.js'
 
 const router = express.Router();
 
 router.post('/register',registerUser)
+router.post('/login',loginUser)
+router.get('/find/:userId',finduser)
+router.get('/',getalluser)
 
 export default router;
--- Server/src/controllers/userControls.ts
+++ Server/src/controllers/userControls.ts
@@ -16,6 +16,7 @@ const registerUser = async(req:Request,res:Response)=>{
     try {
         const {name,email,password} = req.body;
         let user = await userModel.findOne({email})
+        //Checks
         if (user) {
             return res.status(400).json("user with the given email already exit...")
         }
@@ -39,7 +40,51 @@ const registerUser = async(req:Request,res:Response)=>{
         console.log(error);
         res.status(500).json(error)
     }
-
 };
 
-export {registerUser}
+const loginUser = async(req:Request,res:Response)=>{
+    try {
+        const {email,password} = req.body;
+        let user = await userModel.findOne({email})
+        const isValidPassword = await bcrypt.compare(password,user.password)
+        //Checks
+        if (!email || !password){
+            return res.status(400).json("Please fill all fields..")
+        }
+        if (!user) {
+            return res.status(400).json("Invalid Email or Password...")
+        }
+        if (!isValidPassword) {
+            return res.status(400).json("Invalid Email or Password...")
+        }
+    
+        const token = createToken(user._id);
+        res.status(200).json({_id:user._id,email,token})
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+};
+
+const finduser= async(req:Request,res:Response) => {
+    try {
+        const userId = req.params.userId;
+        const user = await userModel.findById(userId)
+        res.status(200).json(user)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
+
+const getalluser= async(req:Request,res:Response) => {
+    try {
+        const users = await userModel.find()
+        res.status(200).json(users)
+    } catch (error) {
+        console.log(error);
+        res.status(500).json(error)
+    }
+}
+
+export {registerUser,loginUser,finduser,getalluser}
