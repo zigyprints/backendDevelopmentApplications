c3f3614d7809b78c407768008cf8128b418f7f65
navvvgupta <abhi2017gupta@gmail.com>
2023-10-04 23:48:00 +0530

    messages-routes

--- models/User.js
+++ models/User.js
@@ -1,38 +1,73 @@
 const mongoose = require('mongoose');
-const {}=require('validator');
-const bcrypt=require('bcrypt');
+const {isEmail} = require('validator');
+const bcrypt = require('bcrypt');
 
-const UserSchema=new mongoose.Schema({
-    name:{
-        type:String,
-        required:[true,'Please provide your name']
-    },
-    email:{
-        type:String,
-        required:[true,'Please provide your email'],
-        unique:true,
-        validate:[validator.isEmail,'Please provide a valid email'],
-        index:true
-    },
-    password:{
-        type:String,
-        required:[true,'Please provide a password'],
-        minlength:6
-    },
-    picture:{
-        type:String,
-        default:'https://res.cloudinary.com/douqbebwk/image/upload/v1628168483/avatar/avatar_cugq40.png'
-    },
-    newMessages:{
-        type:Object,
-        default:{}
-    },
-    status:{
-        type:String,
-        default:'online'
-    },
-},{minimize:false});
+const UserSchema = new mongoose.Schema({
+  name: {
+    type: String,
+    required: [true, "Can't be blank"]
+  },
+  email: {
+    type: String,
+    lowercase: true,
+    unique: true,
+    required: [true, "Can't be blank"],
+    index: true,
+    validate: [isEmail, "invalid email"]
+  },
+  password: {
+    type: String,
+    required: [true, "Can't be blank"]
+  },
+  picture: {
+    type: String,
+  },
+  newMessages: {
+    type: Object,
+    default: {}
+  },
+  status: {
+    type: String,
+    default: 'online'
+  }
+}, {minimize: false});
 
-const User = mongoose.model('User',UserSchema);
+UserSchema.pre('save', function(next){
+  const user = this;
+  if(!user.isModified('password')) return next();
 
-module.exports=User;
+  bcrypt.genSalt(10, function(err, salt){
+    if(err) return next(err);
+
+    bcrypt.hash(user.password, salt, function(err, hash){
+      if(err) return next(err);
+
+      user.password = hash
+      next();
+    })
+
+  })
+
+})
+
+
+UserSchema.methods.toJSON = function(){
+  const user = this;
+  const userObject = user.toObject();
+  delete userObject.password;
+  return userObject;
+}
+
+UserSchema.statics.findByCredentials = async function(email, password) {
+  const user = await User.findOne({email});
+  if(!user) throw new Error('invalid email or password');
+
+  const isMatch = await bcrypt.compare(password, user.password);
+  if(!isMatch) throw new Error('invalid email or password')
+  return user
+}
+
+
+const User = mongoose.model('User', UserSchema);
+
+module.exports = User
