65cc8c6a4c5281d6db8dc3c85a19efc6e4613013
Anshu Kumar <anshukumar@Anshus-MacBook.local>
2023-10-08 01:32:07 +0530

    Implemented image sharing and tested apis

--- script.js
+++ script.js
@@ -2,13 +2,17 @@ const socket = io("http://localhost:3000");
 const messageContainer = document.getElementById("message-container");
 const messageForm = document.getElementById("send-container");
 const messageInput = document.getElementById("message-input");
+const imageInput = document.getElementById("image-input");
 
 const name = prompt("What is your name?");
 appendMessage("You joined");
 socket.emit("new-user", name);
 
 socket.on("chat-message", (data) => {
-  appendMessage(`${data.name}: ${data.message}`);
+  appendMessage(`${data.senderName}: ${data.message}`);
+  if (data.image) {
+    appendImage(data.image);
+  }
 });
 
 socket.on("user-connected", (name) => {
@@ -22,13 +26,22 @@ socket.on("user-disconnected", (name) => {
 messageForm.addEventListener("submit", (e) => {
   e.preventDefault();
   const message = messageInput.value;
+  const imageData = imageInput.files[0];
   appendMessage(`You: ${message}`);
-  socket.emit("send-chat-message", message);
+
+  socket.emit("send-chat-message", { message, roomId, imageData });
   messageInput.value = "";
+  imageInput.value = "";
 });
 
 function appendMessage(message) {
   const messageElement = document.createElement("div");
   messageElement.innerText = message;
   messageContainer.append(messageElement);
+}
+
+function appendImage(imageSrc) {
+  const imageElement = document.createElement("img");
+  imageElement.src = imageSrc;
+  messageContainer.append(imageElement);
 }
--- server.js
+++ server.js
@@ -2,6 +2,7 @@ const express = require("express");
 const http = require("http");
 const socketIo = require("socket.io");
 const bodyParser = require("body-parser");
+const multer = require("multer");
 
 const app = express();
 const server = http.createServer(app);
@@ -12,10 +13,16 @@ app.use(bodyParser.json());
 const users = {};
 const rooms = {};
 
-app.use((err, req, res, next) => {
-  console.error(err.stack);
-  res.status(500).send("Something went wrong!");
+const storage = multer.diskStorage({
+  destination: (req, file, cb) => {
+    cb(null, "uploads/");
+  },
+  filename: (req, file, cb) => {
+    cb(null, Date.now() + "-" + file.originalname);
+  },
 });
+
+const upload = multer({ storage });
 
 io.on("connection", (socket) => {
   socket.on("new-user", (name) => {
@@ -33,12 +40,20 @@ io.on("connection", (socket) => {
     }
   });
 
-  socket.on("send-chat-message", ({ message, roomId }) => {
+  socket.on("send-chat-message", async ({ message, roomId, imageData }) => {
     if (rooms[roomId]) {
       const senderName = users[socket.id].name;
       const chatMessage = { message, senderName };
-      rooms[roomId].messages.push(chatMessage);
-      io.to(roomId).emit("chat-message", chatMessage);
+
+      if (imageData) {
+        const imageFileName = imageData.filename;
+        const imageSrc = `/uploads/${imageFileName}`;
+        chatMessage.image = imageSrc;
+        io.to(roomId).emit("chat-message", chatMessage);
+      } else {
+        rooms[roomId].messages.push(chatMessage);
+        io.to(roomId).emit("chat-message", chatMessage);
+      }
     } else {
       socket.emit("room-not-found", "Room not found");
     }
@@ -65,7 +80,7 @@ io.on("connection", (socket) => {
 
 app.post("/create-room", (req, res) => {
   const { roomName } = req.body;
-  const roomId = Date.now().toString(); // Generate a unique room ID
+  const roomId = Date.now().toString();
   rooms[roomId] = { name: roomName, users: [], messages: [] };
   res.json({ success: true, roomId });
 });
@@ -86,6 +101,15 @@ app.get("/get-messages/:roomId", (req, res) => {
     res.json({ success: true, messages });
   } else {
     res.status(404).json({ success: false, message: "Room not found" });
+  }
+});
+
+app.post("/upload-image", upload.single("image"), (req, res) => {
+  if (req.file) {
+    const imageUrl = `/uploads/${req.file.filename}`;
+    res.json({ success: true, imageUrl });
+  } else {
+    res.status(400).json({ success: false, message: "Image upload failed" });
   }
 });
 
